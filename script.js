ef69ca9861786ff3c9382c5bf7667cff600c7811
Mkaif-Qureshi <quremkaif7@gmail.com>
2024-05-20 15:14:05 +0530

    Firebase Fixed and minor design changes

--- script.js
+++ script.js
@@ -32,3 +32,30 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const timelineItems = document.querySelectorAll(".timeline-item");
+
+  window.addEventListener("scroll", function() {
+    timelineItems.forEach(item => {
+      if (isElementInViewport(item)) {
+        item.querySelector(".timeline-content").classList.add("active");
+      }
+    });
+  });
+
+  function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+});
+
+
+
+// messages
+  
